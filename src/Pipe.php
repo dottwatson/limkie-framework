a7eae7d90170500d73e4bd3502292247a3675351
mirko.temperini@gmail.com <admin@euve254219.serverprofi24.net>
2020-05-25 17:36:05 +0200

    some improvement of pipe

--- src/Pipe.php
+++ src/Pipe.php
@@ -8,6 +8,18 @@ class Pipe{
 
     protected $value;
 
+    /**
+     * The flow followed by the variable
+     *
+     * @var array
+     */
+    protected $pipeFlow = [];
+
+    /**
+     * Initialize object with variable to pipe
+     *
+     * @param mixed $value
+     */
     public function __construct($value){
         $this->value = $value;
 
@@ -18,13 +30,24 @@ class Pipe{
 
 
     /**
-     * Enable chianer on undefined object functions to global functions
+     * Enable chianer on undefined object functions to global functions.
+     * This is invoked by pipe method too
      *
      * @param string|array|Closure $name
      * @param  $args
      * @return void
      */
     public function __call($name,$args){
+        $this->pipeFlow[] = [
+            'pipe'          => $name,
+            'args'          => $args,
+            'prev_value'    => (!is_object($this->value)) ? $this->value: clone $this->value,
+            'current_value' => null,
+            'executed'      => false
+        ];
+
+        $pipeFlowKey = count($this->pipeFlow) -1; 
+
         if(is_callable($name)){
             if(empty($args)){
                 $args[] = PIPE_VALUE;
@@ -36,7 +59,13 @@ class Pipe{
                 }
             }
         
+
+
             $this->value = call_user_func_array($name,$args);
+
+            $this->pipeFlow[$pipeFlowKey]['executed']       = true;
+            $this->pipeFlow[$pipeFlowKey]['current_value']  = (!is_object($this->value)) ? $this->value : clone $this->value;
+
             return $this;
         }
 
@@ -61,6 +90,16 @@ class Pipe{
      */
     public function get(){
         return $this->value;
+    }
+
+    /**
+     * Returns an array of detailed pipes , 
+     * where each pipe haves the pipe action, pipe args, executed status and value (before and after pipe)
+     *
+     * @return array
+     */
+    public function getFlow(){
+        return $this->pipeFlow;
     }
 
 }
